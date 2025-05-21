0fa52fe9cdf0488380c8b89cf5d5375bd0910d0e
SimoneDeMarchi <simone.demarchi@outlook.com>
2025-05-21 15:53:36 +0300

    [Fix] CartesianGrid stroke

--- genzai/src/charts/AIToolBar.jsx
+++ genzai/src/charts/AIToolBar.jsx
@@ -46,7 +46,7 @@ function AIToolsBar({ data }) {
             <div className='tag'>AI Tools Usage</div>
             <ResponsiveContainer width="100%" height={300}>
                 <BarChart data={barData} margin={{ top: 20, right: 30, left: 20, bottom: 40 }}>
-                    <CartesianGrid strokeDasharray="3 3" />
+                    <CartesianGrid strokeDasharray="3 3" stroke='#404040' />
                     <XAxis
                         dataKey="name"
                         stroke="#CFCFCF"
